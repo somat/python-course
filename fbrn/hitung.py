1490c6d5e6ce2bad17fe7026514afb072b2d3e68
Aries Febrian <aries@febrian.(none)>
2011-10-13 15:34:39 +0700

    perbaikan

--- fbrn/hitung.py
+++ fbrn/hitung.py
@@ -17,8 +17,11 @@ pengurangan=c-d
 perkalian=e*f
 pembagian=g/h
 
-print "operasi penambahan"
-print penambahan
+print "operasi penambahan 4+4="
+print penambahan 
+print "operasi Pengurangan 10-5="
 print pengurangan
+print "operasi perkalian 5x5="
 print perkalian
+print "operasi pembagian 25:5="
 print pembagian
